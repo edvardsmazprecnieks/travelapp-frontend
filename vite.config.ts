985ae3a229d1ddfaebf781d0d89411e7a3ab2beb
edvardsmazprecnieks <edvards.mazprecnieks@code.berlin>
2026-05-04 05:33:00 +0200

    fix for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
 	plugins: [react()],
 	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"),
+		},
 		dedupe: ["react", "react-dom"],
 	},
+
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:3000",
+				target: "https://travel-backend-6qda.onrender.com",
 				changeOrigin: true,
-				secure: command !== "serve",
+				secure: true,
 			},
 		},
+	},
+	build: {
+		outDir: "dist",
+		sourcemap: false,
 	},
 }));
